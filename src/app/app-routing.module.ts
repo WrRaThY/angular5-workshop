9185c9db4b3f7ad2ff6139abfa11a93d3516a24f
WrRaThY <woohaas@gmail.com>
2018-03-24 13:01:21 +0100

    added two components and routing for them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {SearchComponent} from './tv/search/search.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }, {
+        path: 'tv',
+        component: SearchComponent
+    }, {
+        path: 'contact',
+        component: ContactComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import {AppRoutingModule} from './app-routing.module';
 
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { SearchComponent } from './tv/search/search.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        ContactComponent,
+        SearchComponent
     ],
     imports: [
         BrowserModule,
